7f8a2c2bff4c0cf22807c9f2c62a5a99b7a99779
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-12-21 23:02:10 -0300

    começando a implementar autenticação

---
+++ src/views/login/Auth.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="view-wrapper">
+      <h1>Login</h1>
+      <button @click="back()">Voltar</button>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    name: "LoginView",
+    methods: {
+      back() {
+        return this.$router.push("/");
+      },
+    },
+  });
+  </script>
+  
+  <style lang="scss" scoped>
+  .view-wrapper {
+    height: 100%;
+  }
+  </style>
+  
