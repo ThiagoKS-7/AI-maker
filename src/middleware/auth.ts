2ce28684b7e652ce77b22ac377af21f44355d7e4
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-12-21 23:02:18 -0300

    #39-release: começando a implementar autenticação

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,38 +1,50 @@
-import firebaseConfig from '../firebase';
-import { getAuth, signInWithPopup, signOut, GoogleAuthProvider, TwitterAuthProvider, GithubAuthProvider } from "firebase/auth";
-firebaseConfig
+import firebaseConfig from "../firebase";
+import {
+  getAuth,
+  signInWithPopup,
+  signOut,
+  GoogleAuthProvider,
+  TwitterAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
+firebaseConfig;
 const provider = new GoogleAuthProvider();
 const providerTwitter = new TwitterAuthProvider();
 const providerGithub = new GithubAuthProvider();
 const auth = getAuth();
 export function handleSignInGoogle() {
-    signInWithPopup(auth, provider)
-    .then((result) => {          
-        console.log(result.user.displayName);
-    }).catch((error) => {
-        console.log(error);
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result.user.displayName);
+    })
+    .catch((error) => {
+      console.log(error);
     });
-};
+}
 export function handleSignInTwitter() {
-    signInWithPopup(auth, providerTwitter)
-    .then((result) => {          
-        console.log(result.user.displayName);
-    }).catch((error) => {
-        console.log(error);
+  signInWithPopup(auth, providerTwitter)
+    .then((result) => {
+      console.log(result.user.displayName);
+    })
+    .catch((error) => {
+      console.log(error);
     });
-};
+}
 export function handleSignInGitHub() {
-    signInWithPopup(auth, providerGithub)
-    .then((result) => {          
-        console.log(result.user.displayName);
-    }).catch((error) => {
-        console.log(error);
+  signInWithPopup(auth, providerGithub)
+    .then((result) => {
+      console.log(result.user.displayName);
+    })
+    .catch((error) => {
+      console.log(error);
     });
-};
+}
 export function handleSignOut() {
-    signOut(auth).then(() => {
-        console.log("out")
-    }).catch((error) => {
-        console.log(error);
+  signOut(auth)
+    .then(() => {
+      console.log("out");
+    })
+    .catch((error) => {
+      console.log(error);
     });
 }
